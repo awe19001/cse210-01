91977c23831dd1c6b8e3f036be4412f0ae1955d3
joeverlyn <joeverlyncapute@gmail.com>
2022-11-17 17:09:22 -0700

    submit

--- seeker-incomplete/seeker-incomplete/seeker/game/seeker.py
+++ seeker-incomplete/seeker-incomplete/seeker/game/seeker.py
@@ -19,17 +19,35 @@ def __init__(self):
         Args:
             self (Seeker): An instance of Seeker.
         """
-        self.location = random.randint(1, 1000)
-        self.distance = [0, 0] # start with two so get_message always works
-      
+        self._location = random.randint(1, 1000)
+        self._distance = [0, 0] # start with two so get_message always works
+     
+     
+def get_message(self):
+        """Gets a message from the seeker.
+        Args:
+            self (Seeker): An instance of Seeker.
+        
+        Returns:
+            string: A message from the seeker.
+        """
+        message = "\nI'm going to find you!"
+        if self._distance[-1] == 0:
+            message = "\nI'm going to find you!"
+        elif self._distance[-1] < self._distance[-2]:
+            message = "\nShhh. I'm sneaking in now..."
+        elif self._distance[-1] > self._distance[-2]:
+            message = "\nI'm running around, but I'll find you..."
+        return message
+     
 # 3) Create the get_location(self) method. Use the following method comment.
-        self.location = random.randint(1, 1000)
+def get_location(self):
         """Gets the current location.
         
         Returns:
             number: The current location,
-        """
-        self.distance = [0, 0]
+        """      
+        return (self._distance[-1] == 0)        
         
 # 4) Create the move_location(self, location) method. Use the following method comment.
 def move(self, location):
@@ -39,8 +57,7 @@ def move(self, location):
             self (Seeker): An instance of Seeker.
             location (int): The given location.
         """
-        distance = abs(self.location - location)
-        self.distance.append(distance)
-        self.location = location
-
+        distance = abs(self._location - location)
+        self._distance.append(distance)
+        self._location = location
 
